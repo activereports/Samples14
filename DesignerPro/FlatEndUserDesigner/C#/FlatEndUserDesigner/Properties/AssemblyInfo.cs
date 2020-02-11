ae8b5f9ce089e5eb069dc79030eb0a82b17017b8
Roman Shapiro <netframeworker@gmail.com>
2020-02-11 16:26:24 +0700

    Added FlatEndUserDesigner sample

---
+++ DesignerPro/FlatEndUserDesigner/C#/FlatEndUserDesigner/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EndUserDesigner")]
+[assembly: AssemblyCompany("GrapeCity")]
+[assembly: AssemblyProduct("EndUserDesigner")]
+[assembly: AssemblyCopyright("Copyright(c) 2019 GrapeCity, inc")]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly:Guid("E550208B-1010-4F51-A488-2E05F64E3AD1")]
+
+
+//Version information for an assembly consists of the following four values:
+
+//	 Major Version
+//	 Minor Version 
+//	 Build Number
+//	 Revision
+//
+//You can specify all the values or you can default the Revision and Build Numbers 
+//by using the '*' as shown below:
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
