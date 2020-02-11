28e4b9970ebc4459b44d5db3bb58216f14767150
Roman Shapiro <netframeworker@gmail.com>
2020-02-11 16:26:24 +0700

    Added FlatEndUserDesigner sample

---
+++ DesignerPro/FlatEndUserDesigner/C#/FlatEndUserDesigner/ExportViewerFactory.cs
@@ -0,0 +1,14 @@
+﻿using GrapeCity.ActiveReports.Design.Advanced;
+using GrapeCity.ActiveReports.Viewer.Win.Internal.Export;
+using GrapeCity.ActiveReports.Win.Export;
+
+namespace GrapeCity.ActiveReports.Samples.FlatUserDesigner
+{
+	internal class ExportViewerFactory : IExportViewerFactory
+	{
+		public IExportViewer CreateExportViewer(Viewer.Win.Viewer viewer)
+		{
+			return new ExportViewer(viewer);
+		}
+	}
+}
