d729f600f84a0833666e6ad49a26c3057c2e7bae
Roman Shapiro <netframeworker@gmail.com>
2020-02-11 16:26:24 +0700

    Added FlatEndUserDesigner sample

---
+++ DesignerPro/FlatEndUserDesigner/C#/FlatEndUserDesigner/Program.cs
@@ -0,0 +1,27 @@
+﻿using GrapeCity.ActiveReports.Design.Advanced;
+using System;
+using System.Windows.Forms;
+
+namespace GrapeCity.ActiveReports.Samples.FlatUserDesigner
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var designerForm = new DesignerForm
+			{
+				ExportViewerFactory = new ExportViewerFactory(),
+				SessionSettingsStorage = new SessionSettingsStorage()
+			};
+
+			Application.Run(designerForm);
+		}
+	}
+}
---
+++ DesignerPro/FlatEndUserDesigner/C#/FlatEndUserDesigner/SessionSettingsStorage.cs
@@ -0,0 +1,36 @@
+﻿using GrapeCity.ActiveReports.Design.Advanced;
+
+namespace GrapeCity.ActiveReports.Samples.FlatUserDesigner
+{
+	internal class SessionSettingsStorage : ISessionSettingsStorage
+	{
+		public void Load(SessionSettings settings)
+		{
+			settings.RecentFiles = Settings.Default.RecentFiles;
+			settings.WindowSize = Settings.Default.WindowSize;
+			settings.WindowPosition = Settings.Default.WindowPosition;
+			settings.IsWindowMaximized = Settings.Default.IsWindowMaximized;
+			settings.RightPanelWidth = Settings.Default.RightPanelWidth;
+			settings.ActiveRightTab = Settings.Default.ActiveRightTab;
+			settings.LeftPanelWidth = Settings.Default.LeftPanelWidth;
+			settings.IsGroupEditorActive = Settings.Default.IsGroupEditorActive;
+			settings.IsReportExplorerActive = Settings.Default.IsReportExplorerActive;
+			settings.IsLayersListActive = Settings.Default.IsLayersListActive;
+		}
+
+		public void Save(SessionSettings settings)
+		{
+			Settings.Default.RecentFiles = settings.RecentFiles;
+			Settings.Default.WindowSize = settings.WindowSize;
+			Settings.Default.WindowPosition = settings.WindowPosition;
+			Settings.Default.IsWindowMaximized = settings.IsWindowMaximized;
+			Settings.Default.RightPanelWidth = settings.RightPanelWidth;
+			Settings.Default.ActiveRightTab = settings.ActiveRightTab;
+			Settings.Default.LeftPanelWidth = settings.LeftPanelWidth;
+			Settings.Default.IsGroupEditorActive = settings.IsGroupEditorActive;
+			Settings.Default.IsReportExplorerActive = settings.IsReportExplorerActive;
+			Settings.Default.IsLayersListActive = settings.IsLayersListActive;
+			Settings.Default.Save();
+		}
+	}
+}
